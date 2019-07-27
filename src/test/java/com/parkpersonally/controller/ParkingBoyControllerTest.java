672906f51e90f744906ab85ff95306a7c6b8128d
peterxkl <32321026+peterxkl@users.noreply.github.com>
2019-07-27 18:23:00 +0800

    finish test:getAllParkingLotOnParkingBoy Controlle

---
+++ src/test/java/com/parkpersonally/controller/ParkingBoyControllerTest.java
@@ -0,0 +1,58 @@
+package com.parkpersonally.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.parkpersonally.exception.NoSuchParkingBoyException;
+import com.parkpersonally.model.ParkingBoy;
+import com.parkpersonally.model.ParkingLot;
+import com.parkpersonally.model.Tag;
+import com.parkpersonally.service.ParkingBoyService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+public class ParkingBoyControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @MockBean
+    private ParkingBoyService service;
+
+    @Test
+    public  void should_return_all_parkingLot_when_getAllParkingLotOnParkingBoy() throws Exception {
+
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(new ParkingLot(000001l,"停车场1",100,18));
+        parkingLots.add(new ParkingLot(000002l,"停车场2",100,20));
+
+        given(service.getAllParkingLotOnParkingBoy(100000l)).willReturn(parkingLots);
+
+        mockMvc.perform(get("/parking-boys/{parkingBoyId}/parking-lots",100000l))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2));
+
+    }
+
+}
