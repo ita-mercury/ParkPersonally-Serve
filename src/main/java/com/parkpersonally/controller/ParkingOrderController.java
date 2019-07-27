0798c89e45ddef5d8fba8d1e2a4d233462766468
Amo <Panzihua9701@qq.com>
2019-07-27 16:21:28 +0800

    feat: implement ParkingController createOrder

--- src/main/java/com/parkpersonally/controller/ParkingOrderController.java
+++ src/main/java/com/parkpersonally/controller/ParkingOrderController.java
@@ -1,6 +1,9 @@
 package com.parkpersonally.controller;
 
 import com.parkpersonally.model.ParkingOrder;
+import com.parkpersonally.service.ParkingBoyService;
+import com.parkpersonally.service.ParkingOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +13,12 @@ import javax.validation.Valid;
 @RestController
 public class ParkingOrderController {
 
-    @PostMapping("/parking-orders")
-    public ParkingOrder createOrder(@Valid @RequestBody ParkingOrder order){
+    @Autowired
+    private ParkingOrderService parkingOrderService;
 
-        return null;
+    @PostMapping("/parking-orders")
+    public ParkingOrder createOrder(@Valid @RequestBody ParkingOrder order) {
+
+        return parkingOrderService.createParkingOrder(order);
     }
 }
