ead7683f0c5849fd0029abde85fa4e32b71c637b
Amo <Panzihua9701@qq.com>
2019-07-28 12:59:42 +0800

    feat: ParkingBoyService add findOneById

--- src/main/java/com/parkpersonally/service/ParkingBoyService.java
+++ src/main/java/com/parkpersonally/service/ParkingBoyService.java
@@ -23,4 +23,12 @@ public class ParkingBoyService {
         ParkingBoy parkingBoy = parkingBoyRepository.findById(parkingBoyId).orElseThrow(()->new NoSuchParkingBoyException("抱歉,没有查到停车员"));
         return parkingBoy.getParkingLots();
     }
+
+    public ParkingBoy findOneById(long id){
+        ParkingBoy parkingBoy = parkingBoyRepository.findById(id).orElseThrow(
+                () -> new NoSuchParkingBoyException("没有找到ParkingBoy信息")
+        );
+
+        return parkingBoy;
+    }
 }
