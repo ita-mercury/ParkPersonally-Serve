961fa583ad70356d7c036688fc3a8ce85c7cfd9f
Amo <Panzihua9701@qq.com>
2019-07-29 22:15:23 +0800

    add GlobalExceptionHandler handleUpdateParkingLotCapacitySmallerException

--- src/main/java/com/parkpersonally/controller/GlobalExceptionHandler.java
+++ src/main/java/com/parkpersonally/controller/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.parkpersonally.controller;
 
 import com.parkpersonally.exception.*;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.ConstraintViolationException;
@@ -11,45 +12,51 @@ public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(GetParkingOrderException.class)
-    String handleGetParkingOrderException(GetParkingOrderException ex){
-        return  ex.getMessage();
+    public String handleGetParkingOrderException(GetParkingOrderException ex){
+        return ex.getMessage();
     }
 
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoSuchParkingBoyException.class)
-    String handleNoSuchParkingBoyException(NoSuchParkingBoyException ex){
+    public String handleNoSuchParkingBoyException(NoSuchParkingBoyException ex){
         return  ex.getMessage();
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoSuchParkingOrderException.class)
-    String handleNoSuchParkingOrderException(NoSuchParkingOrderException ex ){
+    public String handleNoSuchParkingOrderException(NoSuchParkingOrderException ex ){
         return ex.getMessage();
     }
 
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ParkingLotIsFullException.class)
-    String handleParkingLotIsFullException(ParkingLotIsFullException ex ){
+    public String handleParkingLotIsFullException(ParkingLotIsFullException ex ){
         return ex.getMessage();
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ConstraintViolationException.class)
-    String handleConstraintViolationException(ConstraintViolationException ex ){
+    public String handleConstraintViolationException(ConstraintViolationException ex ){
         return ex.getMessage();
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(CreateParkingOrderException.class)
-    String handleCreateParkingOrderException(CreateParkingOrderException ex ){
+    public String handleCreateParkingOrderException(CreateParkingOrderException ex ){
         return ex.getMessage();
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoSuchManagerException.class)
-    String handleNoSuchManagerException(NoSuchManagerException ex){
+    public String handleNoSuchManagerException(NoSuchManagerException ex){
         return  ex.getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UpdateParkingLotCapacitySmallerException.class)
+    public String handleUpdateParkingLotCapacitySmallerException(UpdateParkingLotCapacitySmallerException ex ){
+        return ex.getMessage();
     }
 }
--- src/main/java/com/parkpersonally/exception/UpdateParkingLotCapacitySmallerException.java
+++ src/main/java/com/parkpersonally/exception/UpdateParkingLotCapacitySmallerException.java
@@ -1,4 +1,10 @@
 package com.parkpersonally.exception;
 
-public class UpdateParkingLotCapacitySmallerException {
+public class UpdateParkingLotCapacitySmallerException extends RuntimeException {
+    public UpdateParkingLotCapacitySmallerException() {
+    }
+
+    public UpdateParkingLotCapacitySmallerException(String message) {
+        super(message);
+    }
 }
