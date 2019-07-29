0d5dcf5f0a2e44b98b80f7a850632822a2e6daaa
LIUJE7 <LIUJE7@oocl.cpm>
2019-07-29 15:54:16 +0800

    add some class

---
+++ src/main/java/com/parkpersonally/repository/ManagerRepository.java
@@ -0,0 +1,7 @@
+package com.parkpersonally.repository;
+
+import com.parkpersonally.model.Manager;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ManagerRepository extends JpaRepository<Manager,Long> {
+}
---
+++ src/test/java/com/parkpersonally/controller/ManagerControllerTest.java
@@ -0,0 +1,4 @@
+package com.parkpersonally.controller;
+
+public class ManagerControllerTest {
+}
