481bccbfcb50a8c18c44332fd53c086e34a1e441
Amo <Panzihua9701@qq.com>
2019-07-29 16:41:23 +0800

    feat: GlobalExceptionHafdle add handleCreateParkingOrderException & fix: naming of GlobalExceptionHafdle

--- src/main/java/com/parkpersonally/controller/GlobalExceptionHandler.java
+++ src/main/java/com/parkpersonally/controller/GlobalExceptionHandler.java
@@ -1,9 +1,6 @@
 package com.parkpersonally.controller;
 
-import com.parkpersonally.exception.GetParkingOrderException;
-import com.parkpersonally.exception.NoSuchParkingBoyException;
-import com.parkpersonally.exception.NoSuchParkingOrderException;
-import com.parkpersonally.exception.ParkingLotIsFullException;
+import com.parkpersonally.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,33 +11,39 @@ public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(GetParkingOrderException.class)
-    String handleGetParkingOrderException(GetParkingOrderException getParkingOrderException){
-        return  getParkingOrderException.getMessage();
+    String handleGetParkingOrderException(GetParkingOrderException ex){
+        return  ex.getMessage();
     }
 
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoSuchParkingBoyException.class)
-    String handleNoSuchParkingBoyException(NoSuchParkingBoyException noSuchParkingBoyException){
-        return  noSuchParkingBoyException.getMessage();
+    String handleNoSuchParkingBoyException(NoSuchParkingBoyException ex){
+        return  ex.getMessage();
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoSuchParkingOrderException.class)
-    String handleNoSuchParkingOrderException(NoSuchParkingOrderException noSuchParkingOrderException ){
-        return noSuchParkingOrderException.getMessage();
+    String handleNoSuchParkingOrderException(NoSuchParkingOrderException ex ){
+        return ex.getMessage();
     }
 
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ParkingLotIsFullException.class)
-    String handleParkingLotIsFullException(ParkingLotIsFullException parkingLotIsFullException ){
-        return parkingLotIsFullException.getMessage();
+    String handleParkingLotIsFullException(ParkingLotIsFullException ex ){
+        return ex.getMessage();
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ConstraintViolationException.class)
-    String handleConstraintViolationException(ConstraintViolationException constraintViolationException ){
-        return constraintViolationException.getMessage();
+    String handleConstraintViolationException(ConstraintViolationException ex ){
+        return ex.getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(CreateParkingOrderException.class)
+    String handleCreateParkingOrderException(CreateParkingOrderException ex ){
+        return ex.getMessage();
     }
 }
