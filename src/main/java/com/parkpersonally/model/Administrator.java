260f91e793fdb1fc8333ac8761e6a5a10701bb84
peterxkl <32321026+peterxkl@users.noreply.github.com>
2019-07-30 15:09:53 +0800

    create Administrator.java AdministratorController.java AdministratorRepository.java AdministratorService.java

---
+++ src/main/java/com/parkpersonally/model/Administrator.java
@@ -0,0 +1,43 @@
+package com.parkpersonally.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+@Entity
+@Data
+public class Administrator {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @NotNull
+    @Column(nullable = false)
+    private String name;
+
+    // todo password validate
+    private String password;
+
+    @NotNull
+    @Column(nullable = false)
+    private String number;
+
+    @Column(nullable = false)
+    @NotNull
+    @Size(min = 5, max = 13)
+    private String phone;
+
+    @OneToMany
+    private List<ParkingBoy> parkingBoys;
+
+    @OneToMany
+    private List<ParkingLot> parkingLots;
+
+    @OneToMany
+    private List<Manager> managers;
+
+    public Administrator() {
+    }
+}
---
+++ src/main/java/com/parkpersonally/service/AdministratorService.java
@@ -0,0 +1,7 @@
+package com.parkpersonally.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdministratorService {
+}
