5f991f2738303b09ebfbbb6589a8d3a9f673a7d3
Chance <gdchhkf@163.com>
2019-07-31 15:15:08 +0800

    [feat] add Spring Security in the project

--- src/main/java/com/parkpersonally/repository/ParkingBoyRepository.java
+++ src/main/java/com/parkpersonally/repository/ParkingBoyRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ParkingBoyRepository extends JpaRepository<ParkingBoy, Long> {
+    ParkingBoy findParkingBoyByNameEqualsAndAndPasswordEquals(String name, String password);
 }
