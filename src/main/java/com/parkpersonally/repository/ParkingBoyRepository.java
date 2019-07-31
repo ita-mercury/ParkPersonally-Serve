98f2ecbd16dfaedfcc402849f6fbc6925cfd6f0b
Chance <gdchhkf@163.com>
2019-07-31 18:24:09 +0800

    [feat] maybe cause error

--- src/main/java/com/parkpersonally/repository/ParkingBoyRepository.java
+++ src/main/java/com/parkpersonally/repository/ParkingBoyRepository.java
@@ -3,6 +3,7 @@ package com.parkpersonally.repository;
 import com.parkpersonally.model.ParkingBoy;
 import com.parkpersonally.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 @Repository
 public interface ParkingBoyRepository extends JpaRepository<ParkingBoy, Long> {
     ParkingBoy findParkingBoyByNameEqualsAndAndPasswordEquals(String name, String password);
+    @Query("select pb from ParkingBoy pb where pb.tags in :inputTags")
+    List<ParkingBoy> findParkingBoyContainsTags(List<Tag> inputTags);
+
 }
